f34f5d1eda9807a64eec12a24697f2475cbd5afe
Joonsoo Kim <daniel.kim365@hotmail.com>
2019-10-09 20:38:37 -0400

    changed json files

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ class App extends Component {
 
   getResumeData(){
     $.ajax({
-      url:'/resumeData.json',
+      url:'https://github.com.io/resumeData.json',
       dataType:'json',
       cache: false,
       success: function(data){
