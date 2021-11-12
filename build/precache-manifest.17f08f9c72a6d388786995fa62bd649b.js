503b670c37828cf8aedf7dab7179b9d3add19656
Joonsoo Kim <daniel.kim365@hotmail.com>
2021-11-12 13:17:55 -0500

    new changes

---
+++ build/precache-manifest.17f08f9c72a6d388786995fa62bd649b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f455e7e6f7b30eead47dac8aa5b98f73",
+    "url": "/personal_website/index.html"
+  },
+  {
+    "revision": "99de8100a891c3af7620",
+    "url": "/personal_website/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "5f721c34e42b7c5bf672",
+    "url": "/personal_website/static/js/2.0145ea27.chunk.js"
+  },
+  {
+    "revision": "99de8100a891c3af7620",
+    "url": "/personal_website/static/js/main.6bbb79dc.chunk.js"
+  },
+  {
+    "revision": "5695ec128b7014afc3d4",
+    "url": "/personal_website/static/js/runtime-main.cbf75c59.js"
+  }
+]);
