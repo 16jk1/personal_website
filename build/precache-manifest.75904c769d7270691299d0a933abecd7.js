b9a6929e2f35b10e8f38341a0e66e1d42d877303
Joonsoo Kim <daniel.kim365@hotmail.com>
2026-02-08 20:59:21 -0500

    added new ui design

---
+++ build/precache-manifest.75904c769d7270691299d0a933abecd7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7af719097d6899c80e760037a2224aa5",
+    "url": "/personal_website/index.html"
+  },
+  {
+    "revision": "2cb7b386e3fed9df2811",
+    "url": "/personal_website/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "d1e2c7833598c182e40b",
+    "url": "/personal_website/static/js/2.70ec7230.chunk.js"
+  },
+  {
+    "revision": "2cb7b386e3fed9df2811",
+    "url": "/personal_website/static/js/main.9ef6131b.chunk.js"
+  },
+  {
+    "revision": "5695ec128b7014afc3d4",
+    "url": "/personal_website/static/js/runtime-main.cbf75c59.js"
+  }
+]);
